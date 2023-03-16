eee1f814722672e4b69a30c10fa296154f75897a
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-03-16 02:00:55 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 5000;
 const host = 'localhost';
 const router = require('./routes');
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.listen(port, host, () => {
